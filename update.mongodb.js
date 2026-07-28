ee300ce681808fdc1f64be41346e6ff9567dd555
Sai varshith <saivarshith202006@gmail.com>
2026-07-28 18:40:06 +0530

    Uncomment MongoDB updateMany queries for products

--- update.mongodb.js
+++ update.mongodb.js
@@ -1,16 +1,16 @@
 use ('ecommerce');
 
-// db.products.updateMany(
-//     {category:"Electronics"},//first select the documents which you want to update. Here we are selecting all products in the "Electronics" category
-//     {$inc:{price: 100}}
-// ); //updateMany() method is used to update multiple documents. Here we are updating the price of all products in the "Electronics" category by $100
+ db.products.updateMany(
+     {category:"Electronics"},//first select the documents which you want to update. Here we are selecting all products in the "Electronics" category
+     {$inc:{price: 100}}
+); //updateMany() method is used to update multiple documents. Here we are updating the price of all products in the "Electronics" category by $100
 
 //set is for replacing the older values
 //inc is for incrementing the older values
 
-// db.products.updateMany(
-//     {category:"Electronics"},//first select the documents which you want to update. Here we are selecting all products in the "Electronics" category
-//     {$set:{category: "Gadgets"}}
-// );
+ db.products.updateMany(
+     {category:"Electronics"},//first select the documents which you want to update. Here we are selecting all products in the "Electronics" category
+     {$set:{category: "Gadgets"}}
+ );
 
 
